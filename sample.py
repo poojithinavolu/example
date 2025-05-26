2ffee6938e3d4039a0e71332321908a01fedd8b5
poojithinavolu <poojith1216@gmail.com>
2025-05-26 11:02:49 +0530

    add

--- sample.py
+++ sample.py
@@ -1,3 +1,11 @@
 print('hello')
 for i in range(10):
+    if i == 5:
+        continue
+    if i == 8:
+        break
+    if i % 2 == 0:
+        print('even')
+    else:
+        print('odd')
   print('i : ',i)
